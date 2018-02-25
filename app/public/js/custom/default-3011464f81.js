4900a9b8462589f72216388cd61f7028865d1662
AnsonKong <kongweian@163.com>
2018-02-25 11:56:02 +0800

    uglify js & minify css

--- app/public/js/custom/default-3011464f81.js
+++
@@ -1,4 +0,0 @@
-function getCsrf() {
-  const keyValue = document.cookie.match('(^|;) ?csrfToken=([^;]*)(;|$)');
-  return keyValue ? keyValue[2] : null;
-}
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 const gulp = require('gulp');
 const del = require('del');
 const rev = require('gulp-rev');
+const revFormat = require('gulp-rev-format');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify-es').default;
+const pump = require('pump');
 const revCollector = require("gulp-rev-collector");
 
 const cssDist = 'app/public/css/custom';
@@ -13,30 +17,42 @@ gulp.task('clean', function(cb) {
     cb();
 });
 
-gulp.task('devCss', function() {
-	var stream = gulp.src(['app/view/src/css/*.css'])
-        .pipe(rev())
-        .pipe(gulp.dest(cssDist))
-        .pipe(rev.manifest({
-        	path: 'rev-css-manifest.json'
-        }))
-        .pipe(gulp.dest('build/rev'));
-    return stream;
+gulp.task('devCss', function(cb) {
+	pump([
+        gulp.src('app/view/src/css/*.css'),
+        cleanCSS({compatibility: 'ie8'}),
+        rev(),
+        revFormat({
+            prefix: '.',
+            suffix: '.min',
+            lastExt: false
+        }),
+        gulp.dest(cssDist),
+        rev.manifest({ path: 'rev-css-manifest.json' }),
+        gulp.dest('build/rev')
+        ], cb);
 });
 
-gulp.task('devJs', function() {
-    var stream = gulp.src(['app/view/src/js/*.js'])
-        .pipe(rev())
-        .pipe(gulp.dest(jsDist))
-        .pipe(rev.manifest({
-            path: 'rev-js-manifest.json'
-        }))
-        .pipe(gulp.dest('build/rev'));
-    return stream;
+gulp.task('devJs', ['devCss'], function(cb) {
+    pump([
+        gulp.src('app/view/src/js/*.js'),
+        uglify(),
+        rev(),
+        revFormat({
+            prefix: '.',
+            suffix: '.min',
+            lastExt: false
+        }),
+        gulp.dest(jsDist),
+        rev.manifest({ path: 'rev-js-manifest.json' }),
+        gulp.dest('build/rev')
+        ], cb);
 });
 
 gulp.task('default', ['clean', 'devCss', 'devJs'], () => {
     gulp.src(['build/rev/*.json', 'app/view/src/**/*.tpl'])
-            .pipe(revCollector())
+            .pipe(revCollector({
+                revSuffix: '.[0-9a-f]{8,10}.min?'
+            }))
             .pipe(gulp.dest(tplDist));
 });
