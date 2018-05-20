09c5cd23cae4925898253d1c85ee1085688e0f3b
AnsonKong <kongweian@163.com>
2018-05-20 18:33:32 +0800

    comment notfound-handler alertMsg

--- app/middleware/notfound-handler.js
+++ app/middleware/notfound-handler.js
@@ -3,7 +3,7 @@ module.exports = () => {
   return async (ctx, next) => {
     await next();
     if (ctx.status === 404) {
-      ctx.service.router.storeAlertMsg(`您所访问的路径${ctx.path}不存在。`);
+      // ctx.service.router.storeAlertMsg(`您所访问的路径${ctx.path}不存在。`);
       ctx.redirect('/');
     }
   };
